0d606297b33f0231ac8c016612c95ea72eeb717c
Elias Riveros <elias4845@gmail.com>
2020-09-29 23:05:35 -0400

    Create directory with the name of the project and create a README.md file inside the directory.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import string
+import os
+import time
 
 MIN_NAME_LENGTH = 2
 MAX_NAME_LENGTH = 30
 
+#valid chars are uppercase letter, lowercase letters, numbers and - _
 VALID_CHARS = (
     list(string.ascii_uppercase) +
     list(string.ascii_lowercase) +
@@ -32,18 +35,33 @@ def is_valid_author(author):
 if __name__ == "__main__":
     project_name = input("What is your project name?\n")
 
+
     while not is_valid_project_name(project_name):
         project_name = input("Invalid project name. Try again\n")
-
-    #if not is_valid_project_name(project_name):
-    #    print("Invalid project name. Exiting")
-    #    exit(0)
 
     author = input("Who is the author of this project?\n")
 
     while not is_valid_author(author):
         author = input("Invalid author. Try again\n")
 
+    #the path of the directory is going to be one above the cwd
+    output_dir_path = os.path.join("..", project_name)
+
+    try:
+        os.mkdir(output_dir_path)
+        print("Created project directory at %s" %output_dir_path)
+    except OSError as e:
+        print("Couldn't create directory: ", e)
+        exit(0)
+
+    readme = ("#"+project_name+"\n\n##Description\n\nTODO\n\n##How to install the project\n\nTODO\n\n##Author\n"+author)
+
+    #the path of the README.md file is going to be inside the output_dir_path
+    readme_output_path = os.path.join(output_dir_path, "README.md")
+
+    with open(readme_output_path, 'w+') as f:
+        f.write(readme)
+    
     print("PROJECT DETAILS\n")
     print("Project name: ", project_name)
     print("Author: ", author)
